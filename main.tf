debc0dae6124e866329807a2ac9074f4d12cb342
Alexis VIALARET <alexis.vialaret@artefact.com>
2023-08-23 09:40:09 +0200

    upd: doc

--- main.tf
+++ main.tf
@@ -2,10 +2,18 @@
 * # terraform-google-airbyte-flows
 * A Terraform module to programmatically deploy end-to-end ELT flows to BigQuery on Airbyte.
 *
+* ## Prerequisites
+*
+* - An up and running Airbyte instance on GCP
+*   - [Deploy Airbyte on compute engine](https://docs.airbyte.com/deploying-airbyte/on-gcp-compute-engine/)
+*   - [Deploy Airbyte on Kubernetes using Helm](https://docs.airbyte.com/deploying-airbyte/on-kubernetes-via-helm)
+*
 * ## Usage
 *
 * ###[Basic configuration example](./examples/basic/main.tf)
+*
 * ###[YAML configuration example](./examples/yaml/main.tf)
+*
 * This module is designed to be compatible with external YAML configuration files. It is a convenient way for users not proficient in Terraform to specify/modify ELT pipelines programmatically, or to integrate this module with other tools that can generate YAML files.
 *
 * ### YAML Configuration reference:
